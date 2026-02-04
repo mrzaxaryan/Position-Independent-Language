ad1065a12b9fcffa406af859cf4e22cfb128abcd
mrzaxaryan <mrzaxaryan>
2026-02-04 11:32:52 -0800

    Remove Windows platform-specific runtime components
    - Deleted kernel32.cc, memory.cc, ntdll.cc, pe.cc, peb.cc, platform.cc, socket.cc, random.cc files.
    - These files contained platform-specific implementations for memory allocation, event handling, and socket operations.
    - The removal of these files is part of a larger refactor to streamline the runtime and improve cross-platform compatibility.

--- src/runtime/platform/windows/kernel32.cc
+++
@@ -1,5 +0,0 @@
-#include "kernel32.h"
-#include "platform.h"
-#include "peb.h"
-
-#define ResolveKernel32ExportAddress(functionName) ResolveExportAddressFromPebModule(Djb2::HashCompileTime(L"kernel32.dll"), Djb2::HashCompileTime(functionName))
